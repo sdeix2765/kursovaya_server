0e8eb327a25012a4cf914921fca71202a10a3ebe
sdeix <test@test.ru>
2024-05-18 10:20:54 +0300

    all funtional was maded

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,4 +14,67 @@ class ApiController extends Controller
     {
        return DB::table('room_class')->get();
     }
+    public function reviews()
+    {
+       return DB::table('reviews')->get();
+    }
+    
+    public function rooms()
+    {
+       return DB::table('rooms')->get();
+    }
+    public function addreview(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required',  
+            'message' => 'required|max:500'
+        ]);
+        DB::table('reviews')->insert([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'message' => $validatedData['message'],
+        ]);
+    
+        return response()->json(['message' => 'Добавлен новый отзыв']);
+    }
+    public function addguest(Request $request)
+    {
+        $validatedData = $request->validate([
+            'first_name' => 'required|max:30',
+            'last_name' => 'required|max:30',  
+            'patronymic' => 'max:30',
+            'number' => 'required|min:11|max:11',
+            'email'=>'required|max:50'
+        ]);
+        $id =DB::table('guests')->insertGetId([
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
+            'patronymic' => $validatedData['patronymic'],
+            'number' => $validatedData['number'],
+            'email' => $validatedData['email'],
+        ]);
+    
+        return response()->json(['message' => 'Добавлено', 'guest_id'=>$id]);
+    }
+    
+    public function addbook(Request $request)
+    {
+        $validatedData = $request->validate([
+            'room_id' => 'required',
+            'guest_id' => 'required',  
+            'guest_count' => 'required',
+            'time_from' => 'required',
+            'time_to'=>'required'
+        ]);
+        DB::table('bookings')->insert([
+            'room_id' => $validatedData['room_id'],
+            'guest_id' => $validatedData['guest_id'],
+            'guest_count' => $validatedData['guest_count'],
+            'time_from' => $validatedData['time_from'],
+            'time_to' => $validatedData['time_to'],
+        ]);
+    
+        return response()->json(['message' => 'Добавлено']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,22 @@ Route::get('/images/{image}', function ($image) {
 
 // user routes
 Route::get('/room_classes', [\App\Http\Controllers\ApiController::class,'room_classes'])->name('room_classes');
+Route::get('/rooms', [\App\Http\Controllers\ApiController::class,'rooms'])->name('rooms');
+Route::get('/reviews', [\App\Http\Controllers\ApiController::class,'reviews'])->name('reviews');
+Route::post('/addreview', [\App\Http\Controllers\ApiController::class,'addreview'])->name('addreview');
+Route::post('/addguest', [\App\Http\Controllers\ApiController::class,'addguest'])->name('addguest');
+Route::post('/addbook', [\App\Http\Controllers\ApiController::class,'addbook'])->name('addbook');
+
+
+
+
+
 
 // admin routes
 Route::middleware(correcttoken::class)->post('/addroom_class', [\App\Http\Controllers\ApiAdminController::class,'addroom_class'])->name('addroom_class');
 Route::middleware(correcttoken::class)->post('/addroom', [\App\Http\Controllers\ApiAdminController::class,'addroom'])->name('addroom');
+
+
 
 Route::post('/register', [\App\Http\Controllers\APIauthController::class,'register'])->name('register');
 Route::post('/login', [\App\Http\Controllers\APIauthController::class,'login'])->name('login');
